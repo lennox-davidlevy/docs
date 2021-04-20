d9374fa55bcc1e36f6b0e2fe486504044036bd02
David Levy <lennox.davidlevy@gmail.com>
2021-04-20 14:23:49 -0400

    mongo added

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -13,9 +14,31 @@ const TOOLBAR_OPTIONS = [
   ["clean"],
 ];
 
+const SAVE_INTERVAL_MS = 2000;
+
 const TextEditor = () => {
   const [socket, setSocket] = useState();
   const [quill, setQuill] = useState();
+  const { id: documentId } = useParams();
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    socket.once("load-document", (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
+    socket.emit("get-document", documentId);
+  }, [socket, quill, documentId]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+    const interval = setInterval(() => {
+      socket.emit("save-document", quill.getContents());
+    }, SAVE_INTERVAL_MS);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [socket, quill]);
 
   useEffect(() => {
     const s = io("http://localhost:3001");
@@ -59,6 +82,8 @@ const TextEditor = () => {
         toolbar: TOOLBAR_OPTIONS,
       },
     });
+    q.disable();
+    q.setText("Loading...");
     setQuill(q);
   }, []);
 
